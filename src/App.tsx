aaea6a5298eabc1db0949f492d9c190f30f4c289
hieutom663 <hieutom663@gmail.com>
2025-11-08 18:03:48 +0700

    update be dang nhap, chua api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import "./App.css";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import Register from "./components/Register";
 import Login from "./components/Login";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Link to={"/login"}>Đăng nhập</Link> */}
+      <Link to={"/login"}>Đăng nhập</Link>
       <Routes>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
